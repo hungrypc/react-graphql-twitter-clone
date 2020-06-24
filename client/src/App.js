4c5ca221f8e2b529553a9af21b9eeb28ab788ae4
hpc <philipchanhk626@gmail.com>
2020-06-24 19:00:12 -0400

    auth page skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import './style/css/App.css';
+import { Router, Route, Switch } from 'react-router-dom';
 
+import Explore from './pages/Explore'
+import Auth from './pages/Auth'
+
+import history from './modules/history'
+
+import './style/css/App.css';
 require('dotenv').config()
 
 function App() {
-
   return (
-    <div className="App">
-      app
-    </div>
+    <Router history={history}>
+      <div className="App">
+        <Switch>
+          <Route path="/" component={Explore} exact />
+          <Route path="/login" component={Auth} exact />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
 function Auth() {
+
+  const submitHandler = (event) => {
+    console.log('submit')
+  }
+
   return (
-    <div>
-      
+    <div className="login">
+      <div className="login__container">
+        <form>
+          <label for="username">Username</label>
+          <input type="text" name="username" autoComplete="off"></input>
+          <label for="password">Password</label>
+          <input type="password" name="password"></input>
+          <button>Log in</button>
+        </form>
+      </div>
     </div>
   )
 }
